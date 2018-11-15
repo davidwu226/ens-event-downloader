483b558340071771bf14863bd788531583fea2d8
Dave <dave@chatmost.com>
2018-11-15 12:03:24 -0800

    Added concurrency.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Web3 from 'web3'
 import fs from 'fs'
 import moment from 'moment'
 import 'moment-duration-format'
+import commander from 'commander'
 import readline from 'readline'
 import artifact from './abi/Registrar.json'
 
@@ -11,92 +12,126 @@ const MAX_RETRIES = 30
 const INITIAL_DELAY = 100
 const MAX_DELAY = 30 * 1000
 const BATCH_SIZE = 25000
-const START = 6500000//3625000
 
-const status = {
-  startTime: new Date(),
-  duration: 0,
-  startBlock: START,
-  endBlock: undefined,
-  fromBlock: 0,
-  blocksProcessed: 0,
-  batchSize: BATCH_SIZE,
-  reduceBatchSize: 0,
-  eventsFound: 0,
-  currentBlock: 0,
-  rate: 0,
-  events : {},
-}
-
-let web3
-
-async function refreshWeb3() {
+async function refreshWeb3(web3) {
   for (let i = 0; i < MAX_RETRIES; ++i) {
     try {
       if (web3 && web3.eth.net.isListening()) {
         return web3
       }
-      
+
       web3 = new Web3(PROVIDER_URL)
-      
+
     } catch (err) {
       // IGNORE.
     }
 
-    await Promise.delay(Math.min(INITIAL_DELAY * (2**i), MAX_DELAY))
+    await Promise.delay(Math.min(INITIAL_DELAY * (2 ** i), MAX_DELAY))
   }
 }
 
-async function updateStatus() {
-  const duration = (new Date()).getTime() - status.startTime.getTime()
-  status.duration = moment.duration(duration, 'ms').format('h[hrs]:m[m]:s[s.]S')
-  status.currentBlock = await web3.eth.getBlockNumber()
-  status.blocksProcessed = status.fromBlock - status.startBlock
-  status.blocksPerSecond = status.blocksProcessed / (duration / 1000)
-}
+class Downloader {
 
-async function start() {
-  await refreshWeb3()
-  const registry = await web3.eth.ens.checkNetwork()
-  const registrar = await web3.eth.ens.registry.owner('eth')
-  const Registrar = new web3.eth.Contract(artifact.abi, registrar)
-  for (status.fromBlock = status.startBlock; status.fromBlock < (status.endBlock || await web3.eth.getBlockNumber()); status.fromBlock += status.batchSize) {
-    while (true) {
-      try {
-        const s = new Date()
-        const fromBlock = status.fromBlock
-        const toBlock = status.fromBlock + status.batchSize - 1
-        const events = await Registrar.getPastEvents('allEvents', { fromBlock, toBlock })
-        const e = new Date()
-        const d = e.getTime() - s.getTime()
-        status.lastDuration = moment.duration(d, 'ms').format('h[hrs]:m[m]:s[s.]S')
-        status.lastBlocksPerSecond = status.batchSize / (d / 1000)
-        status.eventsFound += events.length
-        events.forEach(e => {
-          status.events[e.event] = (status.events[e.event] || 0) + 1
-        })
-        fs.writeFileSync(`ens-events-${fromBlock}-${toBlock}`, JSON.stringify(events, null, 2))
-        break
-      } catch (err) {
-        console.log(err)
-        status.batchSize = status.batchSize / 2
-        status.reduceBatchSize++
-        console.log(`Reducing batch size to ${status.batchSize}.`)
-      }
+  constructor(id, start, end) {
+    this.status = {
+      id,
+      startTime: new Date(),
+      duration: 0,
+      startBlock: start,
+      endBlock: end,
+      fromBlock: 0,
+      blocksProcessed: 0,
+      batchSize: BATCH_SIZE,
+      reduceBatchSize: 0,
+      eventsFound: 0,
+      currentBlock: 0,
+      rate: 0,
+      events: {},
     }
   }
-  status.completed = true
+
+  async updateStatus() {
+    const duration = (new Date()).getTime() - this.status.startTime.getTime()
+    this.status.duration = moment.duration(duration, 'ms').format('h[hrs]:m[m]:s[s.]S')
+    this.status.currentBlock = await this.web3.eth.getBlockNumber()
+    this.status.blocksProcessed = this.status.fromBlock - this.status.startBlock
+    this.status.blocksPerSecond = this.status.blocksProcessed / (duration / 1000)
+  }
+
+  async start() {
+    this.web3 = await refreshWeb3(this.web3)
+    
+    const registry = await this.web3.eth.ens.checkNetwork()
+    const registrar = await this.web3.eth.ens.registry.owner('eth')
+    const Registrar = new this.web3.eth.Contract(artifact.abi, registrar)
+    
+    for (this.status.fromBlock = this.status.startBlock; this.status.fromBlock < (this.status.endBlock || await this.web3.eth.getBlockNumber()); this.status.fromBlock += this.status.batchSize) {
+      while (true) {
+        try {
+          const s = new Date()
+          const fromBlock = this.status.fromBlock
+          const toBlock = this.status.fromBlock + this.status.batchSize - 1
+          const events = await Registrar.getPastEvents('allEvents', { fromBlock, toBlock })
+          const e = new Date()
+          const d = e.getTime() - s.getTime()
+          this.status.lastDuration = moment.duration(d, 'ms').format('h[hrs]:m[m]:s[s.]S')
+          this.status.lastBlocksPerSecond = this.status.batchSize / (d / 1000)
+          this.status.eventsFound += events.length
+          events.forEach(e => {
+            this.status.events[e.event] = (this.status.events[e.event] || 0) + 1
+          })
+          fs.writeFileSync(`ens-events-${fromBlock}-${toBlock}`, JSON.stringify(events, null, 2))
+          break
+        } catch (err) {
+          console.log(err)
+          this.status.batchSize = this.status.batchSize / 2
+          this.status.reduceBatchSize++
+          console.log(`Reducing batch size to ${this.status.batchSize}.`)
+          this.web3 = await refreshWeb3(this.web3)
+        }
+      }
+    }
+    this.status.completed = true
+  }
 }
 
-readline.emitKeypressEvents(process.stdin);
-process.stdin.setRawMode(true);
+const downloaders = []
+
+async function main() {
+  commander
+  .option('-s --start <n>', 'Start block', parseInt)
+  .option('-e --end <n>', 'End block', parseInt)
+  .option('-c --concurrency <n>', 'Number of concurrent downloaders', parseInt)
+  .parse(process.argv)
+
+  const web3 = await refreshWeb3()
+  const start = commander.start === undefined ? 3625000 : commander.start
+  const end = commander.end || (await web3.eth.getBlockNumber())
+  const concurrency = commander.concurrency || 1
+  const path = commander.args[0] || './ens-events-' 
+
+  console.log(`Starting from ${start} to ${end} with concurrency ${concurrency} writing to '${path}'.`)
+  const division = Math.ceil((end - start) / concurrency)
+
+  let id = 0
+  for (let i = start; i < end; i += division) {
+    const downloader = new Downloader(id++, i, i + division - 1)
+    downloader.start()
+    downloaders.push(downloader)
+  }
+}
+
+readline.emitKeypressEvents(process.stdin)
+process.stdin.setRawMode(true)
 process.stdin.on('keypress', async (str, key) => {
   if (key.ctrl && key.name === 'c') {
     process.exit();
   } else {
-    await updateStatus()
-    console.log(JSON.stringify(status, null, 2))
+    await Promise.each(downloaders, async (downloader) => {
+      await downloader.updateStatus()
+      console.log(JSON.stringify(downloader.status, null, 2))      
+    })
   }
-});
+})
 
-start()
+main()
